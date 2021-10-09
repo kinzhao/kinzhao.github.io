9a7806750cc00fa5dfdb8c83f476575f526054f3
kinzhao <kinsz89@gmail.com>
2021-10-09 01:41:59 -0400

    Update personal details

--- src/lib/home.js
+++ src/lib/home.js
@@ -5,7 +5,7 @@ export function Component() {
         <div class="home">
             <div class="introduction">
                 <h1>Hello, I'm Kin.</h1>
-                <p>I'm a Software Engineer at <a class="mlbam" href="http://www.mlbam.com/">MLBAM</a>.</p>
+                <p>I'm a Senior Software Engineer at <a class="jpmc" href="https://www.chase.com/">JP Morgan Chase</a>.</p>
                 <p>Interested in Web, Mobile Development, Machine Learning, Artificial Intelligence and learning new technologies.</p>
                 <div class="social-media">
                     ${SocialList.map(item => `<a href="${item.link}"><span><i class="fa fa-${item.icon} fa-2x" aria-hidden="true"></i></span></a>`).join(' ')}
